81c06bc0f0d7ce9eea46ac203642dadf77ba4a65
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-11-16 17:13:23 +0530

    Few_Example_of_Control_Constructs

--- 08_Few_Example_of_Control_Constructs.py
+++ 08_Few_Example_of_Control_Constructs.py
@@ -18,7 +18,6 @@ if originalnum == int(newnum):  # Convert 'newnum' back to an integer for compar
 else:
     print("Not a palindrome")  # Print if the number is not a palindrome
 
-
 # ------------------------------------------
 # Check if the entered number is a palindrome using a mathematical approach
 
