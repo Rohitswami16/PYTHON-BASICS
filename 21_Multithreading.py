eaf18d24cdead517d27300eaeedbc0317913101f
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2024-11-28 19:16:19 +0530

    Multithreading in Python

--- 21_Multithreading.py
+++ 21_Multithreading.py
@@ -1,1 +1,43 @@
+import time
+import threading
 
+nums = [1, 2, 3, 4, 5]
+letters = ['a', 'b', 'c', 'd', 'c']
+
+
+def display_digit(nums):
+    for i in nums:
+        print(i)
+        time.sleep(3)
+      
+def display_letters(letters):
+    for i in letters:
+        print(i)
+        time.sleep(3)
+      
+# Create threads, passing the functions and their arguments
+t1 = threading.Thread(target=display_digit, args=(nums,))
+t2 = threading.Thread(target=display_letters, args=(letters,))
+
+# Start the threads
+t1.start()
+t2.start()
+
+# Wait for threads to complete
+t1.join()
+t2.join()
+
+print("Both threads have finished.")
+
+# Excepted Output
+# 1
+# a
+# 2
+# b
+# 3
+# c
+# 4
+# d
+# 5
+# c
+# Both threads have finished.
