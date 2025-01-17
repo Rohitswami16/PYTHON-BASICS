cd59e33fe8be0d72575bb8b308ca7565f414f52b
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2025-01-17 16:23:37 +0530

    Created jerry.py

---
+++ jerry.py
@@ -0,0 +1,19 @@
+age = int(input("Enter your age: "))
+
+
+# nested if statements to determine eligibility for a rollercoaster ride
+if age >= 10:
+    print("You are old enough to ride the rollercoaster")
+    height = int(input("Enter your height in cm: "))
+    if height >= 120:
+        print("And you meet the height requirement.")
+        w = int(input("Enter ur w"))
+        if w > 50:
+            print("enjoy ur ride")
+        else:
+            print("increse ur age")
+
+    else:
+        print("But you do not meet the height requirement.")
+else:
+    print("You are not eligible to ride the rollercoaster because you are too young.")
